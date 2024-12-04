878b4b41bf127941af0333c101ee8d36194ebb3d
Sengeki1 <gamersengeki@gmail.com>
2024-12-04 15:55:49 -0100

    added files

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
 const soap = require("soap")
 
-const wslURL = './RegistoAvaliaService.xml' // XML file received from the route bellow
-const urlServico = "http://localhost:8080/RegistroAvaliaService?wsdl"
+const urlServico = "http://localhost:8080/Servico?wsdl"
 
-const args = {
-    codigoTurma: 1
+const options = {
+    timeout: 60000,  // Set the timeout to 60 seconds
+    wsdl_headers: { 'Connection': 'keep-alive' },  // Ensure persistent connection
+};
+
+const argsAdicionarNavio = {
+    id_navio: 0,
+    nome: "Mindless",   
+    carga_bruta: 55,
+    calado: 55.2
 }
 
-soap.createClient(wslURL, (erro, cliente) => {
+const agrsAdicionarPassageiro = {
+    id_viagem: 0,
+    nome: "Jeffrey Bezos",
+    idade: 55
+}
+
+const agrsAdicionarViagem = {
+    id_viagem: 0,
+    id_navio: 0,
+    porto_partida: "Mindelo",
+    porto_destino: "Porto",
+    hora_partida: 17,
+    hora_chegada: 20
+}
+
+
+soap.createClient(urlServico, options, (erro, cliente) => {
     if (erro) {
         console.error("Erro ao criar objecto cliente", erro);
         return
     }
 
-    cliente.setEndpoint(urlServico)
-
-    cliente.consultarAvaliaTurma(args, (erro, resultado) => {
+    cliente.adicionarNavio(argsAdicionarNavio, (erro, resultado) => {
         if (erro) {
-            console.log("Error")
+            console.log("Erro ao chamar o serviço: ", erro)
         } else {
-            console.log("Resposta do Serviço: ", JSON.stringify(resultado, null, 2))
+            console.log("Resposta do Serviço: ", JSON.stringify(resultado))
         }
-    }) // method from the xml
+    })
+
+    cliente.adicionarViagem(agrsAdicionarViagem, (erro, resultado) => {
+        if (erro) {
+            console.log("Erro ao chamar o serviço: ", erro)
+        } else {
+            console.log("Resposta do Serviço: ", JSON.stringify(resultado))
+        }
+    })
+
+    cliente.adicionarPassageiro(agrsAdicionarPassageiro, (erro, resultado) => {
+        if (erro) {
+            console.log("Erro ao chamar o serviço: ", erro)
+        } else {
+            console.log("Resposta do Serviço: ", JSON.stringify(resultado))
+        }
+    })
 })
